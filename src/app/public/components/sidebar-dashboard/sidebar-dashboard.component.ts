9b5f00e3e3b2ea685c6962ad3add4e54ee039e06
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-15 21:29:09 -0500

    chore : add standalone attribute

--- src/app/public/components/sidebar-dashboard/sidebar-dashboard.component.ts
+++ src/app/public/components/sidebar-dashboard/sidebar-dashboard.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar-dashboard',
   imports: [],
+  standalone: true,              // marca standalone
   templateUrl: './sidebar-dashboard.component.html',
   styleUrl: './sidebar-dashboard.component.css'
 })
